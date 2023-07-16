1fd8c79ea2b6868afbd6c08df094bd651643f8b2
nour-tarabein <108428692+nour-tarabein@users.noreply.github.com>
2023-07-16 14:11:51 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/timeline')
+def timeline():
+    # Mockup data for tweets
+    posts = [
+        {'Majed': 'manager1', 'rate': '30/50'},
+        {'Nizar': 'manager2', 'rate': '6/50!'},
+        {'Nour': 'manager3', 'rate': '17/50'},
+    ]
+
+    return render_template('timeline.html', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
